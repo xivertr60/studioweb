54a0999efbc410313db98cfc8004a6ea82311605
shop <pepigames@outlook.es>
2025-05-19 23:08:31 +0200

    Update dsac.js

--- dsac.js
+++ dsac.js
@@ -1,11 +1,1 @@
-document.getElementById("buscar").addEventListener("click", () => {
-    let usuario = document.getElementById("nombreUsuario").value;
-    
-    fetch("economia.json")
-        .then(response => response.json())
-        .then(data => {
-            let balance = data[usuario] ? data[usuario].balance : "No encontrado";
-            document.getElementById("resultado").innerText = `Balance: ${balance}`;
-        })
-        .catch(error => console.error("Error al obtener datos", error));
-});
+
