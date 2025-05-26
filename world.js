4ca1568cb6c380e8a0e0558b1dca3059d31ab3ff
shop <pepigames@outlook.es>
2025-05-26 20:36:42 +0200

    Delete world.js

--- world.js
+++
@@ -1,9 +0,0 @@
-// world.js
-function createGround(scene) {
-  const geometry = new THREE.BoxGeometry(100, 1, 100);
-  const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-  const ground = new THREE.Mesh(geometry, material);
-  ground.position.y = -0.5;
-  scene.add(ground);
-  return ground;
-}
