656e600366902c5db29378ebbf804629a305a968
shop <pepigames@outlook.es>
2025-05-26 20:36:32 +0200

    Delete player.js

--- player.js
+++
@@ -1,9 +0,0 @@
-// player.js
-function createPlayer(scene) {
-  const playerGeometry = new THREE.BoxGeometry(1, 2, 1);
-  const playerMaterial = new THREE.MeshBasicMaterial({color: 0x0000ff});
-  const player = new THREE.Mesh(playerGeometry, playerMaterial);
-  player.position.y = 1;
-  scene.add(player);
-  return player;
-}
